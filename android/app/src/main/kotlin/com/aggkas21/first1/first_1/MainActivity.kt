346d20420613567bb9ad28d2cc2838678ead1b8d
aggkas21 <aggkas21@gmail.com>
2022-06-19 15:52:37 +0530

    Day3

---
+++ android/app/src/main/kotlin/com/aggkas21/first1/first_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aggkas21.first1.first_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int day = 20;
+    final String name = "Kashish";
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("First App")),
+      ),
+      body:Center(
+        child: Container(
+          child: Text("Hello My First App $day by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
